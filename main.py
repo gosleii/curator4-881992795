7c254dba7d5cd126f90b47a342b28b460cb84932
gosleii <sleii@inbox.ru>
2024-08-07 20:08:11 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import telebot
+
+bot = telebot.TeleBot('7317119146: AAEWOiWAs-mbH7a1cNPpzCHtMkcz1qmGs7g')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, '*Привет*', parse_mode='Markdown')
+
+
+@bot.message_handler(commands=['Yes or No?'])
+def YesorNo(message):
+    bot.send_message(message.chat.id, '_YES_', parse_mode='Markdown')
+
+
+bot.infinity_polling()
